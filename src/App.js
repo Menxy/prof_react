44f90cf7745dca8e587ed7a2b997da9e6d56c58e
Дюша <58334576+Menxy@users.noreply.github.com>
2023-04-23 12:23:20 +0300

    Homework_1 v2.0 Characters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import Simpsons from "./components/simpsons/Simpsons";
-import {simpsons} from "./components/arrays/arrays"
+/*import Simpsons from "./components/simpsons/Simpsons";*/
+/*import {simpsons} from "./components/arrays/arrays"*/
 
 function App() {
   return (
     <>
-      <Simpsons simpsons={simpsons}/>
+      {/*1. Описати всю сім'ю сімпсонів (5 персонажів)*/}
+      {/*<Simpsons simpsons={simpsons}/>*/}
+      {/*2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
+        https://rickandmortyapi.com/
+        https://rickandmortyapi.com/api/character
+        Створити 6 персонажів*/}
+        Characters
     </>
   );
 }

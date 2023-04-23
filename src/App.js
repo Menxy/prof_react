b8e6320d10940a9c9cc04dae851c9e497ba3687e
Дюша <58334576+Menxy@users.noreply.github.com>
2023-04-23 12:06:09 +0300

    Homework_1 v1.1 Simpsons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Simpsons from "./components/simpsons/Simpsons";
-import {simpsons} from "./components/simpsonsArray/SimpsonsArray";
-
+import {simpsons} from "./components/arrays/arrays"
 
 function App() {
   return (

a04c5e4bd4f9cf45bf941e6ae987fdc401776851
Дюша <58334576+Menxy@users.noreply.github.com>
2023-04-26 18:54:32 +0300

    Lesson_3 Form 1.16

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,20 +1,25 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
 import {carService} from "../../services/car.service";
+import {joiResolver} from "@hookform/resolvers/joi"
+import {carValidator} from "../../validators/car.validator";
 
 function CarForm({setAllCars, carUpdate}) {
-    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({mode:'onChange'});
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({
+        mode: 'onChange',
+        resolver: joiResolver(carValidator)
+    });
 
-    useEffect(()=>{
-        if(carUpdate){
-            setValue('brand', carUpdate.brand)
-            setValue('price', carUpdate.price)
-            setValue('year', carUpdate.year)
+    useEffect(() => {
+        if (carUpdate) {
+            setValue('brand', carUpdate.brand, {shouldValidate: true})
+            setValue('price', carUpdate.price, {shouldValidate: true})
+            setValue('year', carUpdate.year, {shouldValidate: true})
         }
-    },[carUpdate])
+    }, [carUpdate])
     const save = async (car) => {
         const {data} = await carService.create(car)
-        setAllCars(prev=>!prev)
+        setAllCars(prev => !prev)
         /*console.log(car)*/
         reset()
     }
@@ -22,30 +27,33 @@ function CarForm({setAllCars, carUpdate}) {
 
     }
     return (
-        <form onSubmit={handleSubmit(carUpdate?update:save)} className={'carItem form'}>
-            <input type="text" placeholder={'brand'} {...register('brand', {
+        <form onSubmit={handleSubmit(carUpdate ? update : save)} className={'carItem form'}>
+            <input type="text" placeholder={'brand'} {...register('brand',
+                /*{
                 pattern: {
                     value: /^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/,
                     message: '1-20 літер'
                 },
-                required: {value:true,message:'Обов\'язково'}
-            })}/>
+                required: {value: true, message: 'Обов\'язково'}
+            }*/
+            )}/>
             {errors.brand && <span>{errors.brand.message}</span>}
-            <input type="text" placeholder={'price'} {...register('price', {
+            <input type="text" placeholder={'price'} {...register('price'/*, {
                 valueAsNumber: true,
                 min: {value: 0, message: 'Більше 0'},
                 max: {value: 1000000, message: 'До 1 млн'},
-                required: {value:true,message:'Обов\'язково'}
-            })}/>
+                required: {value: true, message: 'Обов\'язково'}
+            }*/
+            )}/>
             {errors.price && <span>{errors.price.message}</span>}
             <input type="text" placeholder={'year'} {...register('year', {
                 valueAsNumber: true,
                 min: {value: 1990, message: 'Більше 1990'},
                 max: {value: new Date().getFullYear(), message: 'До 2023'},
-                required: {value:true,message:'Обов\'язково'}
+                required: {value: true, message: 'Обов\'язково'}
             })}/>
             {errors.year && <span>{errors.year.message}</span>}
-            <button className={'carButton'} disabled={!isValid}>Save</button>
+            <button className={'carButton'} disabled={!isValid}>{carUpdate ? 'Update' : 'Create'}</button>
         </form>
     );
 }

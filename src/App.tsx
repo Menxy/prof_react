f967d099926f17fc31f1c5f367c289525b6ffdd6
Дюша <58334576+Menxy@users.noreply.github.com>
2023-04-30 17:19:08 +0300

    homework_3_ts pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import UserPage from "./pages/UserPage";
 import CarPage from "./pages/CarPage";
 import CommentPage from "./pages/CommentPage";
 import Header from "./components/Header/Header";
-import "./App.css"
+
 
 
 const App = () => {
     const [choice, setChoice] = useState<PageEnum>(PageEnum.USERS);
   return (
-      <div className={'canvas'}>
+      <div>
           <Header setChoice={setChoice}/>
           {choice === PageEnum.USERS && <UserPage/>}
           {choice === PageEnum.COMMENTS && <CommentPage/>}

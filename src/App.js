60a5953a21aefb412cadd9ae7bb76e17e999aa0a
Дюша <58334576+Menxy@users.noreply.github.com>
2023-05-15 19:24:36 +0300

    homework_4_js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import Cars from "./components/Cars/Cars";
+
 
 
 function App() {
 
   return (
     <>
-      <Cars/>{/*Реалізувати створення, видалення та оновлення машин*/}
-
-      {/*<Users/>*/}{/*/*Зробити компонент, в якому буде форма, за допомоги якої можливо
-        створити нового юзера постовим запитом на http://jsonplaceholder.typicode.com/users*/}
-      {/*Comments*/}{/*Зробити компонент, в якому буде форма, за допомоги якої можливо
-            створити новий комментар постовим запитом на http://jsonplaceholder.typicode.com/comments*/}
+      Hi!
     </>
   );
 }
